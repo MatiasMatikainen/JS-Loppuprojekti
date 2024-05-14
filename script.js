35f4a23be3f14997a702667b28b056d567bcd9a8
MatiasMatikainen <matias.mm115@gmail.com>
2024-05-15 01:27:39 +0300

    JS tiedostoa muokattu

--- script.js
+++ script.js
@@ -46,6 +46,54 @@ function handleCellClick(clickedCellEvent) {
     }
 }
 
+function handleCellPlayed(clickedCell, clickedCellIndex) {
+    gameState[clickedCellIndex] = currentPlayer;
+    clickedCell.innerHTML = currentPlayer === "Pelajaa" ? "X" : "O";
+}
+
+const winningConditions = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+];
+
+function handleResultValdation() {
+    let roundWon = false;
+    for (let i = 0; i < winningConditions.length; i++) {
+        const winCondition = winningConditions[i];
+        let a = gameState[winCondition[0]];
+        let b = gameState[winCondition[1]];
+        let c = gameState[winCondition[2]];
+        if (a === '' || b === '' || c === '') {
+            continue;
+        }
+        if (a === b && b === c) {
+            roundWon = true;
+            break;
+        }
+    }
+
+    if (roundWon) {
+        statusDisplay.innerHTML = winningText();
+        gameActive = false;
+        return;
+    }
+
+    let roundDraw = !gameState.includes("");
+    if (roundDraw) {
+        statusDisplay.innerHTML = drawText();
+        gameActive = false;
+        return;
+    }
+
+    handlePlayerChange();
+}
+
 function handlePlayerChange() {
     currentPlayer = currentPlayer === "Pelajaa" ? "Tietokone" : "Pelajaa";
     statusDisplay.innerHTML = currentPlayerTurn();
